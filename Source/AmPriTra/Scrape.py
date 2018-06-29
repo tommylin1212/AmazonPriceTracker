20968fc523133b7dee7a5ee6574a7891a5367176
Caleb Tomlin <cwill.i.am.tomlin@gmail.com>
2018-06-28 20:24:15 -0500

    General first steps. -Opens chrome to Amazon (headless if needed) -searches

---
+++ Source/AmPriTra/Scrape.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+DestinationUrl = "http://amazon.com"
+
+
+ChromeOptions = webdriver.ChromeOptions()
+#ChromeOptions.add_argument("--headless")
+ChromeDriverInstance = webdriver.Chrome(executable_path='C:\\Users\Caleb\PycharmProjects\AmazonPriceTracker\Support\chromedriver.exe',
+                                        chrome_options=ChromeOptions)
+# driver.implicitly_wait(30)
+ChromeDriverInstance.get(DestinationUrl)
+
+SearchBar = ChromeDriverInstance.find_element_by_name("field-keywords")  # name of the search bar on amazon.com
+SearchBar.clear()
+SearchBar.send_keys("echo dot")  # easy to replace from csv list
+
+SearchBar.send_keys(Keys.RETURN)
+ChromeDriverInstance.close()
